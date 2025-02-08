0e8c5d4842e443d445e7268e0707ae8a05aaa95e
Franck Lamotte <elviccomte@gmail.com>
2025-02-08 19:19:20 +0100

    Define main scene and some init scripts.

---
+++ scripts/GameMaster.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class GameMaster : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Gamemaster Ready!");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
