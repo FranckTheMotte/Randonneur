af0687d2e122050130aa1fae62412c1c25d89173
Franck Lamotte <elviccomte@gmail.com>
2025-02-22 11:30:07 +0100

    Tutorial 2: Creating User Interfaces.

---
+++ scripts/MainMenu20.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class MainMenu20 : Node2D
+{
+	[Export] public Label myLabel;
+
+	private int count = 0;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		myLabel.Text = "Changed the label on Ready in C#!";
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void _on_button_1_button_up() {
+		count++;
+		myLabel.Text = "The button change the text. It has been clicked: " + count;
+	}
+}
