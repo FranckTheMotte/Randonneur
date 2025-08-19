50acb054da32f19c15c1f0d70c11d485a7af9618
Franck Lamotte <elviccomte@gmail.com>
2025-08-19 09:22:19 +0200

    Add auto walking character on a simple parallax.

---
+++ Scripts/Level/Level1.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class Level1 : Node2D
+{
+	[Export] public Camera2D camera2d;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		// Vector2 inc = new Vector2(camera2d.Offset.X + (100 * (float) delta), camera2d.Offset.Y);
+		// camera2d.Offset = inc;
+	}
+}	
--- Scripts/Testing/PlayerDataTest.cs
+++ Scripts/Testing/PlayerDataTest.cs
@@ -1,18 +1,25 @@
 using Godot;
 using System;
 
-public partial class PlayerDataTest : Node {
+public partial class PlayerDataTest : Node
+{
 
 	[Export] public Label myLabel;
 
-	public override void _Ready() {
+	public override void _Ready()
+	{
 
 		myLabel.Text = "GetTree().CurrentScene.SceneFilePath: " + GetTree().CurrentScene.SceneFilePath + "\n";
 		myLabel.Text += "GameMaster Current Slot " + GameMaster.currentSlotNum + "\n";
 		myLabel.Text += "Sample Dictionary Text: " + GameMaster.playerData.sampleDictionary["test"];
 
 
-    }
+	}
 
+	/* TODO make a scrolling
+	 - load background
+	 - move circulary background with only x forward
+	 - move circulary background with only x forward and y variations
+	 */
 	
 }
