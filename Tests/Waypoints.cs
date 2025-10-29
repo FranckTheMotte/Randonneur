024b801b1025cd1dd3c8cf8c635066fb2eb494dc
Franck Lamotte <elviccomte@gmail.com>
2025-10-29 18:49:32 +0100

    Add simple Waypoint test. Update Tests tree folder.

---
+++ Tests/Waypoints.cs
@@ -0,0 +1,35 @@
+﻿using Randonneur;
+
+namespace TestWaypoints;
+
+public class Tests
+{
+    Waypoint? SimpleWaypoint;
+
+    static readonly float SimpleElevation = 0.0f;
+    static readonly Godot.Vector2 SimpleGeographicCoord = new(1, 1);
+    static readonly string SimpleTraceName = "TraceA";
+
+    [SetUp]
+    public void Setup()
+    {
+        SimpleWaypoint = new("Test")
+        {
+            Elevation = SimpleElevation,
+            GeographicCoord = SimpleGeographicCoord,
+            TraceName = SimpleTraceName,
+        };
+    }
+
+    /// <summary>
+    /// Simple test of Waypoint class
+    /// </summary>
+    [Test]
+    public void SimpleTest()
+    {
+        Assert.That(SimpleWaypoint, Is.Not.Null);
+        Assert.That(SimpleWaypoint?.Elevation, Is.EqualTo(SimpleElevation));
+        Assert.That(SimpleWaypoint?.GeographicCoord, Is.EqualTo(SimpleGeographicCoord));
+        Assert.That(SimpleWaypoint?.TraceName, Is.EqualTo(SimpleTraceName));
+    }
+}
