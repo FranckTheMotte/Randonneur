2f5946ffb8af4c93934c39bc1a6e4cd46867ca87
Franck Lamotte <elviccomte@gmail.com>
2025-12-19 16:52:44 +0100

    feat(debug): add ui for player speed control.

--- Scripts/UI/Actions.cs
+++ Scripts/UI/Actions.cs
@@ -1,3 +1,4 @@
+using System.Security;
 using Godot;
 
 public partial class Actions : CanvasLayer
@@ -5,10 +6,15 @@ public partial class Actions : CanvasLayer
     public override void _Ready()
     {
         // Sanity checks
-        if (Player.Instance != null)
+        if (Player.Instance == null)
         {
-            Player.Instance.Move = true;
+            GD.PushWarning($"${nameof(_on_check_button_toggled)}: sanity checks failed");
+            return;
         }
+
+        Player.Instance.Move = true;
+        SpinBox speed = GetNode<SpinBox>("PlayerSettings/Speed");
+        speed.Value = Player.Instance.Walk;
     }
 
     private void _on_check_button_toggled(bool isToggled)
@@ -22,5 +28,23 @@ public partial class Actions : CanvasLayer
 
         /* Test to start or stop the auto-walk */
         Player.Instance.Move = isToggled;
+    }
+
+    /// <summary>
+    /// Signal to modify the player speed
+    /// </summary>
+    private void _on_speed_value_changed(float Speed)
+    {
+        // Sanity checks
+        if (Player.Instance == null)
+        {
+            GD.PushWarning($"${nameof(_on_check_button_toggled)}: sanity checks failed");
+            return;
+        }
+
+        // retrieve the direction
+        int direction = Player.Instance.Walk >= 0 ? 1 : -1;
+        Player.Instance.Walk = (int)Speed * direction;
+        Player.Instance.Move = true;
     }
 }
