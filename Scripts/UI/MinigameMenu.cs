61aca4f18324d34ad5c2efed3b1565e30fe25de0
Franck Lamotte <elviccomte@gmail.com>
2026-01-24 09:36:21 +0100

    feat(minigame): integrate the picture minigame in the pov waypoint.

--- Scripts/UI/MinigameMenu.cs
+++ Scripts/UI/MinigameMenu.cs
@@ -18,8 +18,54 @@ namespace Randonneur
             None,
         }
 
+        /// <summary>
+        /// Reference to the Picture Game.
+        /// </summary>
+        internal PictureGame? _pictureGameScene;
+
+        Camera2D? _camera;
+
+        public override void _Ready()
+        {
+            _pictureGameScene =
+                GetNode<PictureGame>(
+                    "BgMargin/BgNinePathRect/MapMargin/MapRect/GridContainer/ChoiceContainer/VBoxChoice/PictureGame"
+                ) ?? throw new InvalidOperationException("Failed to find Picture Game scene.");
+
+            // desactivate the camera otherwise, it could conflict with others.
+            Node2D cameraHUD = _pictureGameScene.GetNode<Node2D>("CameraHUD");
+            _camera = cameraHUD.GetNode<Camera2D>("Camera2D");
+            _camera.Enabled = false;
+        }
+
+        /// <summary>
+        /// Normal button.
+        /// </summary>
+        public void _on_normal_button_pressed()
+        {
+            if (_pictureGameScene == null || _camera == null)
+            {
+                GD.PushError("_on_normal_button_pressed(): failure on ressources.");
+                return;
+            }
+
+            _pictureGameScene.Visible = true;
+            _camera.Enabled = true;
+        }
+
+        /// <summary>
+        /// Quit button.
+        /// </summary>
         public void _on_quit_button_pressed()
         {
+            if (_pictureGameScene == null || _camera == null)
+            {
+                GD.PushError("_on_quit_button_pressed(): failure on ressources.");
+                return;
+            }
+
+            _pictureGameScene.Visible = false;
+            _camera.Enabled = false;
             Visible = false;
             // Re-activated the player to let collide with next waypoint collision shape
             if (Player.Instance != null)
